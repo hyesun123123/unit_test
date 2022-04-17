4acaa4d83c561b2ac36056cd5492bc59c6f5e2eb
hyesun123123 <goodnight4857@naver.com>
2022-04-17 10:36:53 +0900

    Test Component interactions

---
+++ src/appContent.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, {shallow, mount} from "enzyme";
+import App from "./App";
+import { ValueInput } from './ValueInput';
+
+Enzyme.configure({adapter: new Adapter()});
+it("Render three ValueInputs",()=>{
+    const wrapper = shallow(<App />);
+    const valCount = wrapper.find(ValueInput).length;
+    expect(valCount).toBe(3)
+})
+it("Fully renders three inputs",()=>{
+    const wrapper = mount(<App title="tester" />);
+    const count = wrapper.find("input.form-control").length
+    expect(count).toBe(3);
+});
+
